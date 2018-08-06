32b9206f8b710abc4fac9bd7e4694b3efcdc7844
Dave Gill <davegill@daves-mbp.home>
2018-08-06 22:04:45 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Nighttime is my first attempt at writing a game in Corona.
+
+I have no idea what I am going to put in this README but we will see.
+
+This project is based on the Corona Project template from Serkan Aksit
---
+++ main.lua
@@ -0,0 +1,9 @@
+
+-- main
+-- Origin. The starting point of your project.
+-- This is the only file Corona SDK will require to work. If it doesn't exist, you'll see an error.
+
+
+local composer = require ("composer")		-- Include the Composer library
+
+composer.gotoScene("scenes.logo", "crossFade", 1000)
