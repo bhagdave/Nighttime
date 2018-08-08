97ea638ab1662e4adf3f17628b81f8234850e99e
Dave Gill <david.g.h.gill@gmail.com>
2018-08-08 20:47:41 +0100

    Changed game level scene to scene template

--- scenes/gameLevel.lua
+++ scenes/gameLevel.lua
@@ -1,96 +1,77 @@
---screens.gameLevel
 
-local composer = require ("composer")       -- Include the Composer library. Please refer to -> http://docs.coronalabs.com/api/library/composer/index.html
-local scene = composer.newScene()           -- Created a new scene
+local composer = require( "composer" )
 
-local mainGroup         -- Our main display group. We will add display elements to this group so Composer will handle these elements for us.
--- For more information about groups, please refer to this guide -> http://docs.coronalabs.com/guide/graphics/group.html
+local scene = composer.newScene()
+
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
 
 
-local fontSize = composer.getVariable( "fontSize" )		-- Get the variable "fontSize" defined in main.lua
 
-local nameDev		-- Forward reference to nameDev
 
-local function changeScene(event)
-	if ( event.phase == "began" ) then
-		print "event began"
-	elseif ( event.phase == "moved" ) then
-		print "event moved"
-    elseif ( event.phase == "ended" or event.phase == "cancelled" ) then 		-- Check if the tap ended or cancelled
-    	print "event ended"
-    	
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
 
-    	-- Define a variable <varName> to nameDev object for further access to transition. You can name it anything.
-    	-- This function will cause nameDev to change its X position and its alpha value in 1500 ms. 
-    	-- After it's done, onComplete will be called and it will change scene.
-    	-- For more information about transitions, please refer to the following documents
-    	-- http://docs.coronalabs.com/api/library/transition/index.html
-    	-- http://docs.coronalabs.com/guide/media/transitionLib/index.html 
-    	nameDev.trans = transition.to( nameDev, {time = 1500, alpha = 0, x = 0, onComplete = function ()
-    			composer.gotoScene( "scenes.mainMenu", "crossFade", 1000 )
-    		end} )
-    end
-    return true 		-- To prevent more than one click
-
-    -- For more information about events, please refer to the following documents
-    -- http://docs.coronalabs.com/api/event/index.html
-    -- http://docs.coronalabs.com/guide/index.html#events-and-listeners
-end
-
-local function cleanUp()
-	-- Remove the Runtime event listener manually. Corona SDK doesn't handle Runtime listeners automatically.
-	-- If not handled, it will probably throw out an error or show some unexpected behavior
-	Runtime:removeEventListener(changeText)
-
-	-- Corona SDK will remove the listeners that are attached to objects if the object is destroyed.
-
-	-- Remove the transition manually.
-	-- if ( nameDev.trans ) then
-	transition.cancel( nameDev.trans )
-	nameDev.trans = nil
-	-- end
-	-- If you are not sure if the nameDev.trans exists or want to take a defensive approach, you can check it with if clause
-end
-
+-- create()
 function scene:create( event )
-    local mainGroup = self.view         -- We've initialized our mainGroup. This is a MUST for Composer library.
+
+    local sceneGroup = self.view
+    -- Code here runs when the scene is first created but has not yet appeared on screen
 
 end
 
 
+-- show()
 function scene:show( event )
+
+    local sceneGroup = self.view
     local phase = event.phase
 
-    if ( phase == "will" ) then         -- Scene is not shown entirely
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is still off screen (but is about to come on screen)
 
-    elseif ( phase == "did" ) then      -- Scene is fully shown on the screen
-    	
+    elseif ( phase == "did" ) then
+        -- Code here runs when the scene is entirely on screen
+
     end
 end
 
 
+-- hide()
 function scene:hide( event )
+
+    local sceneGroup = self.view
     local phase = event.phase
 
-    if ( phase == "will" ) then         -- Scene is not off the screen entirely
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is on screen (but is about to go off screen)
 
-        cleanUp()       -- Clean up the scene from timers, transitions, listeners
-
-    elseif ( phase == "did" ) then      -- Scene is off the screen
+    elseif ( phase == "did" ) then
+        -- Code here runs immediately after the scene goes entirely off screen
 
     end
 end
 
+
+-- destroy()
 function scene:destroy( event )
-    -- Called before the scene is removed
+
+    local sceneGroup = self.view
+    -- Code here runs prior to the removal of scene's view
+
 end
 
--- Listener setup
+
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
 
 return scene
-
--- You can refer to the official Composer template for more -> http://docs.coronalabs.com/api/library/composer/index.html#template
