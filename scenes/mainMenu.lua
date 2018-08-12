183417399905f315e9b4aef986e3d2c2a6e007da
Dave Gill <david.g.h.gill@gmail.com>
2018-08-12 17:27:32 +0100

    Cleaned up menu code

--- scenes/mainMenu.lua
+++ scenes/mainMenu.lua
@@ -8,17 +8,9 @@ local widget = require ("widget")			-- Included the Widget library for buttons, 
 
 
 local mainGroup         -- Our main display group. We will add display elements to this group so Composer will handle these elements for us.
--- For more information about groups, please refer to this guide -> http://docs.coronalabs.com/guide/graphics/group.html
-
 
 local function onButtonRelease (event)		-- This function will be called when the buttons are pressed
-	if ( event.phase == "began" ) then
-		print "event began"
-	elseif ( event.phase == "moved" ) then
-		print "event moved"
-    elseif ( event.phase == "ended" or event.phase == "cancelled" ) then 		-- Check if the tap ended or cancelled
-    	print "event ended"
-
+	if ( event.phase == "ended" or event.phase == "cancelled" ) then 		-- Check if the tap ended or cancelled
         if ( event.target.id == "newGame" ) then
             composer.gotoScene( "scenes.gameLevel", "crossFade", 1000 )
         elseif ( event.target.id == "credits" ) then
@@ -26,23 +18,10 @@ local function onButtonRelease (event)		-- This function will be called when the
         end
     end
     return true 		-- To prevent more than one click
-
-    -- For more information about events, please refer to the following documents
-    -- http://docs.coronalabs.com/api/event/index.html
-    -- http://docs.coronalabs.com/guide/index.html#events-and-listeners
 end
 
 function scene:create( event )
     local mainGroup = self.view         -- We've initialized our mainGroup. This is a MUST for Composer library.
-
-    --[[
-    Remember to insert the background image before other objects on the scene or it will overlapse the other objects
-
-    local menubg = display.newImageRect( "assets/backgroundMenu.jpg", 1280, 720 )
-    menubg.x = display.contentCenterX
-    menubg.y = display.contentCenterY
-    mainGroup:insert(menubg)
-    ]]--
 
     local buttonNewGame = widget.newButton{		-- Creating a new button
         id = "newGame",			-- Give an ID to identify the button in onButtonRelease()
@@ -59,7 +38,6 @@ function scene:create( event )
     buttonNewGame.y = 250
     mainGroup:insert(buttonNewGame)
 
-
     local buttonCredits = widget.newButton{		-- Creating a new button
         id = "credits",			-- Give an ID to identify the button in onButtonRelease()
         label = "CREDITS",
@@ -75,7 +53,6 @@ function scene:create( event )
     buttonCredits.y = 450
     mainGroup:insert(buttonCredits)
 end
-
 
 function scene:show( event )
     local phase = event.phase
