ab51396da01c4c2022cd17fdf059df16535fe95d
Dave Gill <david.g.h.gill@gmail.com>
2018-08-12 17:16:30 +0100

    Added ponywolf stuf for extra functions

--- main.lua
+++ main.lua
@@ -9,5 +9,16 @@ local composer = require ("composer")		-- Include the Composer library
 -- hide status bar
 display.setStatusBar(display.HiddenStatusBar)
 
+-- Removes bottom bar on Android 
+if system.getInfo( "androidApiLevel" ) and system.getInfo( "androidApiLevel" ) < 19 then
+	native.setProperty( "androidSystemUiVisibility", "lowProfile" )
+else
+	native.setProperty( "androidSystemUiVisibility", "immersiveSticky" ) 
+end
+
+-- This module turns gamepad axis events and mobile accelerometer events into keyboard
+-- events so we don't have to write separate code for joystick, tilt, and keyboard control
+require( "com.ponywolf.joykey" ).start()
+
 -- goto splash screen
 composer.gotoScene("scenes.logo", "crossFade", 1000)
