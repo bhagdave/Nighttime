8808475624ad61c5620338e79451210543a4cd62
Dave Gill <david.g.h.gill@gmail.com>
2018-09-01 19:09:43 +0100

    Using the inventory image to display

--- scenes/game/lib/inventory.lua
+++ scenes/game/lib/inventory.lua
@@ -19,13 +19,24 @@ function M.new( options )
 	function group:addInventory(name)
 		table.insert(self.items, {name, "scenes/game/maps/" .. name .. ".png"})
 		for i = 1, table.getn(self.items) do
-			self.items[i] = display.newImageRect( "scenes/game/maps/keyBlue.png", w, h )
+			-- self.items[i] = display.newImageRect( "scenes/game/maps/keyBlue.png", w, h )
+			self.items[i] = display.newImageRect( self:getInventoryImage(name), w, h )
 			self.items[i].x = (i-1) * ( (w/2) + spacing )
 			self.items[i].y = 0
 			group:insert( self.items[i] )
 		end
 		-- group.count = max
 	end
+
+	function group:getInventoryImage(name)
+		for k, v in pairs(self.items) do
+  			if ( v[1] == name ) then
+  				return v[2]
+  			end
+		end
+		return 0
+	end
+
 
 	function group:finalize()
 		-- On remove, cleanup instance 
