5adf30848b2e534d05615be4aa426f5b22c6e2ff
Dave Gill <david.g.h.gill@gmail.com>
2018-09-02 19:25:41 +0100

    Tidy inventory code up

--- scenes/game/lib/inventory.lua
+++ scenes/game/lib/inventory.lua
@@ -17,29 +17,18 @@ function M.new( options )
 	group.items = {}
 
 	function group:addInventory(name)
-		print ("Adding to inventory " .. name)
 		table.insert(self.items, {name, "scenes/game/maps/" .. name .. ".png"})
 		for i = 1, table.getn(self.items) do
 			group:remove(i)
-			print (i)
-			-- self.items[i] = display.newImageRect( "scenes/game/maps/keyBlue.png", w, h )
 			self.items[i].image = display.newImageRect( self:getInventoryImage(i), w, h )
 			self.items[i].image.x = (i-1) * ( (w/2) + spacing )
-			-- self.items[i].x  = (i-1) * 64
 			self.items[i].image.y = 0
 			group:insert( self.items[i].image )
 		end
-		-- group.count = max
 	end
 
 	function group:getInventoryImage(i)
 		return self.items[i][2]
-		-- for k, v in pairs(self.items[i]) do
-  			-- if ( v[1] == name ) then
-  				-- return v[2]
-  			-- end
-		-- end
-		-- return 0
 	end
 
 
